6e8d6741af21f5fc86911ccb0020a19363d0fed0
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-10-29 23:16:30 -0400

    Updates

---
+++ precache-manifest.0ffb12357b369bb0d0a8d0b6cdd8325f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212a1fb1623437ed9773a1ea31a555ce",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "90f5aab7d1e78f03ec04",
+    "url": "/Clicky-Game/static/css/main.78a4383c.chunk.css"
+  },
+  {
+    "revision": "858337689f8457637e8e",
+    "url": "/Clicky-Game/static/js/2.05bcf8b3.chunk.js"
+  },
+  {
+    "revision": "90f5aab7d1e78f03ec04",
+    "url": "/Clicky-Game/static/js/main.e5b5241d.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  }
+]);
